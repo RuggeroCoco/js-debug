220c4925e487216802c1dd80c39634a213f831ee
RuggeroCoco <ruggerococo94@gmail.com>
2023-03-27 18:00:36 +0200

    End of project

---
+++ bundle-2/main.js
@@ -0,0 +1,46 @@
+/*******************************************************************************
+
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+
+    Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
+
+*******************************************************************************/
+
+// ESERCIZIO 1
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// ESERCIZIO 2
+// function addIfEven(num) {
+//   if (num % 2 === 0) {
+//     return num + 5;
+//   }
+//   return num;
+// }
+// console.log(addIfEven(9));
+// console.log(addIfEven(4));
+
+// ESERCIZIO 3
+// function loopToFive() {
+//   for (let i = 0; i <= 5; i++) {
+//     console.log(i);
+//   }
+// }
+// loopToFive();
+
+// ESERCIZIO 4 (suggerimento: ci sono 8 errori)
+// function displayEvenNumbers() {
+//   let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//   let evenNumbers = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//       evenNumbers.push(numbers[i]);
+//     }
+//   }
+//   return evenNumbers;
+// }
+// console.log(displayEvenNumbers()); // [2,4,6,8]
